4a09e3fa1ce3bccb5f40b07c1d108c04d111d153
Qjammer <baladoramiro+github@gmail.com>
2020-07-18 18:09:29 +0200

    first commit

---
+++ conanfile.py
@@ -0,0 +1,59 @@
+from conans import ConanFile, CMake, tools
+
+
+class HelloConan(ConanFile):
+	name = "hello"
+	version = "0.1"
+	settings = "os", "compiler", "build_type", "arch"
+	options = {
+		"shared": [True, False]
+	}
+	default_options = {
+		"shared": False,
+		"boost:shared": True
+	}
+	generators = "cmake"
+	exports_sources = "src*"
+
+	source_folder = "src"
+	install_folder = "install"
+	build_folder = "build"
+	package_folder = "package"
+
+	requires = ["boost/1.72.0"]
+	
+	def configure(self):
+		#self.options['icu'].with_dyload = False
+		pass
+
+
+	def source(self):
+		pass
+
+
+	def build(self):
+		cmake = self.configure_cmake()
+		cmake.build()
+
+		# Explicit way:
+		# self.run('cmake %s/hello %s'
+		#          % (self.source_folder, cmake.command_line))
+		# self.run("cmake --build . %s" % cmake.build_config)
+
+	def package(self):
+		cmake = self.configure_cmake()
+		cmake.install()
+		self.copy("*", src="bin")
+		self.copy("*.h", src="src", dst="include")
+		#self.copy("*.a", dst="lib", keep_path=False)
+		#self.copy("*", src="package/locale", dst="locale")
+
+	def package_info(self):
+		pass
+		#self.cpp_info.libs = ["hello"]
+
+
+	def configure_cmake(self):
+		cmake = CMake(self)
+		cmake.configure(source_folder="src")
+		return cmake
---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#ifdef __cpp_modules
+	import <iostream>;
+	import <filesystem>;
+#else
+	#include  <iostream>
+	#include <filesystem>
+#endif
+
+#include <boost/locale.hpp>
+#include <boost/dll/runtime_symbol_info.hpp>
+
+int main(int, char**) {
+	
+	boost::locale::generator gen;
+	gen.add_messages_domain("hello");
+	{
+	std::string localeDir = std::filesystem::path(boost::dll::program_location().string()).remove_filename() / "locale";
+	gen.add_messages_path(localeDir);
+	}
+
+	auto lang = {"en.UTF-8", "es.UTF-8", "ca.UTF-8", "ru.UTF-8"};
+	
+	for(auto&& l:lang){
+
+		auto g = gen(l);
+		std::locale::global(g);
+		std::cout.imbue(std::locale());
+		std::cout << boost::locale::translate("flashlight") << std::endl;
+		for(auto&& n: {1,2,3,4,5}){
+			std::cout << boost::locale::format(boost::locale::translate("You have deleted {1} file", "You have deleted {1} files", n)) % n << std::endl;
+
+		}
+	}
+	return 0;
+}
